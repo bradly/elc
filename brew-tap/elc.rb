e9e91dbb986d0e544166662cf489d0106ca258f9
Bradly Feeley <4227+bradly@users.noreply.github.com>
2023-08-16 16:40:03 -0700

    Fix distribution url.

--- brew-tap/elc.rb
+++ brew-tap/elc.rb
@@ -2,7 +2,7 @@ class Elc < Formula
   desc       'Edit Last Command Output'
   homepage   'https://github.com/bradly/elc'
   depends_on 'fpp' => :recommended
-  url        'https://github.com/bradly/elc/dist/0.9.0.tar.gz'
+  url        'https://raw.githubusercontent.com/bradly/elc/main/dist/elc-0.9.0.tar.gz'
   sha256     '75f880fd5583073b8603909d627e2de9b3cef9c007e5a0dfe831249308f9ce07'
   option     'without-fpp', 'Do not install fpp'
 
